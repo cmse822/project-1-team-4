a5d70068a5c888a79891b20424c497d6214f83b3
Arman Khoshnevis <khoshne1@msu.edu>
2024-01-16 10:05:21 -0500

    Start the code, randomly initialize the matrix_A

--- project1.cpp
+++ project1.cpp
@@ -1,3 +1,27 @@
 // Project 1 - Matrix Multiplication
 
-// One sample change
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+
+ int main(){
+    // Define number of rows and columns
+    const int row1 = 10;
+    const int column1 = row1;
+    const int row2 = column1;
+    const int column2 = row2;
+    
+    // Define matrices: A * B = C
+    float matrix_A[row1][column1];
+    float matrix_B[row2][column2];
+    float matric_C[row1][column2] = { 0 }; // Initialize all elements to zero
+
+    // Initialize the elements of matrix_A with random values between 0 and 10
+    for (int i = 0; i < row1; ++i) {
+        for (int j = 0; j < column1; ++j) {
+            matrix_A[i][j] = static_cast<float>(std::rand()) / RAND_MAX * 10.0;
+        }
+    }
+    std::cout << matrix_A << std::endl;
+ }
