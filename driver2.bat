6df1152df7bf86eccdf5c4eceea0dd4cf52715bf
ChrisG32123 <chrisg.32123@gmail.com>
2024-01-23 14:04:48 -0500

    Added more runtimes

---
+++ driver2.bat
@@ -0,0 +1,77 @@
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+.\project_2250.exe
+
+echo "Done with 2250"
+
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+.\project_2750.exe
+
+echo "Done with 2750"
+
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+.\project_3250.exe
+
+echo "Done with 3250"
+
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+.\project_3750.exe
+
+echo "Done with 3750"
+
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+.\project_4250.exe
+
+echo "Done with 4250"
+
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+.\project_4750.exe
+
+echo "Done with 4750"
