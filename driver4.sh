205702485fc5f357b6a02bf90010cdeda8ec6265
ChrisG32123 <chrisg.32123@gmail.com>
2024-01-29 18:03:17 -0500

    Different Bash

--- driver4.sh
+++ driver4.sh
@@ -5,10 +5,10 @@ values=(10 25 50 75 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 
 
 # Loop through each value of X
 for value in "${values[@]}"; do
-    # Run the executable multiple times
+    # Run ./project.exe with the current value as an argument 10 times
     for ((i=1; i<=10; i++)); do
-        echo "Running ./project_${value}.exe, iteration $i"
-        ./project_${value}.exe
+        echo "Running ./project.exe with argument $value, iteration $i"
+        ./project.exe "$value"
     done
     echo "Done with $value"
     echo
